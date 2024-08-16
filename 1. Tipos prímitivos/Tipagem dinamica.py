4e6f842df928a8455ae58cb09526801412ab44d9
Aryanxd65 <fabiocarvalhoneves@gmail.com>
2024-08-16 18:14:16 +0000

    Comandos Iniciais .

---
+++ 1. Tipos prímitivos/Tipagem dinamica.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls || clear") 
+
+#Tipagem dinâmica
+
+nome = "Marta"
+idade = 20
+altura = 1.65
+
+print(f"None:  {nome}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
---
+++ 1. Tipos prímitivos/tipos primitivos.py
@@ -0,0 +1,17 @@
+# Tipagem estática.
+nome : str # cadeia
+
+import os
+
+os.system("cls || clear") 
+
+idade : int # inteiro
+altura: float # real
+
+nome = "Marta"
+idade = 20
+altura = 1.65
+
+print(f"None:  {nome}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
